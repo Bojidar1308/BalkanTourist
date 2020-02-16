f2c40290d442fddeea5b7b6a8931c056d760995d
BMarkov <BMarkov@DESKTOP-2OM09KU>
2020-02-16 12:38:42 +0200

    nqma putka ot nuget packages

--- DataBase/Migrations/CodeFirstContextModelSnapshot.cs
+++ DataBase/Migrations/CodeFirstContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace DataBase.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.1")
+                .HasAnnotation("ProductVersion", "3.1.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
